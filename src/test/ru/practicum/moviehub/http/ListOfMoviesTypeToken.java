210537bf8d7641a34080581fa5513050d8f3339f
Anastasia Konts <nastyaaa022@yandex.ru>
2026-04-19 20:04:39 +0700

    Создать MovieHub по тз финального проекта 9, исправить автоматические проверки.

--- src/test/ru/practicum/moviehub/http/ListOfMoviesTypeToken.java
+++ src/test/ru/practicum/moviehub/http/ListOfMoviesTypeToken.java
@@ -4,5 +4,7 @@ import com.google.gson.reflect.TypeToken;
 import ru.practicum.moviehub.model.Movie;
 
 import java.util.List;
+
 public class ListOfMoviesTypeToken extends TypeToken<List<Movie>> {
+
 }
