f846acc54e91b37adddbaa47b4c7e8a94b6383e0
Anastasia Konts <nastyaaa022@yandex.ru>
2026-04-21 18:03:26 +0700

    исправления по результатам ревью. исправление автопроверок

--- src/main/ru/practicum/moviehub/http/MoviesHandler.java
+++ src/main/ru/practicum/moviehub/http/MoviesHandler.java
@@ -14,7 +14,6 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class MoviesHandler extends BaseHttpHandler {
